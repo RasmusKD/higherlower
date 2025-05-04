f830eece549205c4d858c9f4c2e50b41b2a203f4
rasmus123d <59487370+RasmusKD@users.noreply.github.com>
2025-05-04 20:03:49 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,18 @@
 <?php
+// Sæt fejlrapportering til maksimum under udvikling
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Opsæt fejllogging
+ini_set('log_errors', 1);
+ini_set('error_log', dirname(__DIR__) . '/logs/error.log');
+
+// Definer basestien
 define('BASE_PATH', dirname(__DIR__));
+
+// Indlæs router
 require_once BASE_PATH . '/app/routes.php';
 
+// Start routing
 routeRequest();
